65d4847893a766b13a79236c391c542d44e14d9c
MINJE-98 <jmj012100@gmail.com>
2022-09-09 22:23:04 +0900

    feat: 틸로그 이미지용 seed 타입 추가

--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -2,12 +2,13 @@ export type FileType = 'png' | 'jpeg';
 export type Theme = 'light' | 'dark';
 
 export interface ParsedRequest {
-    fileType: FileType;
-    text: string;
-    theme: Theme;
-    md: boolean;
-    fontSize: string;
-    images: string[];
-    widths: string[];
-    heights: string[];
+  seed: string;
+  fileType: FileType;
+  text: string;
+  theme: Theme;
+  md: boolean;
+  fontSize: string;
+  images: string[];
+  widths: string[];
+  heights: string[];
 }
