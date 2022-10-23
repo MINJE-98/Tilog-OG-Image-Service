fefcbeb81d982fe154983b1c9aa49aeaea5aaebb
MINJE-98 <jmj012100@gmail.com>
2022-10-23 12:21:05 +0900

    fix: font

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -9,11 +9,8 @@ const emojify = (text: string) => twemoji.parse(text, twOptions);
 const rglr = readFileSync(
   `${__dirname}/../_fonts/Inter-Regular.woff2`
 ).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
-  'base64'
-);
-const Pretendard = readFileSync(
-  `${__dirname}/../_fonts/Pretendard-Bold.woff2`
+const bold = readFileSync(
+  `${__dirname}/../_fonts/SpoqaHanSansNeo-Bold.woff2`
 ).toString('base64');
 
 function getCss(seed: string, fontSize: string) {
@@ -22,25 +19,12 @@ function getCss(seed: string, fontSize: string) {
   let background = backgroundColor(color);
   let foreground = `${getBrightness(color) ? '#121212' : '#fff'}`;
   return `
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-    }
 
     @font-face {
-        font-family: 'Inter';
+        font-family: 'SpoqaHanSansNeo';
         font-style:  normal;
         font-weight: bold;
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
-
-    @font-face {
-      font-family: 'Pretendard';
-      font-style:  normal;
-      font-weight: bold;
-      src: url(data:font/woff2;charset=utf-8;base64,${Pretendard}) format('woff2');
     }
 
     body {
@@ -90,7 +74,7 @@ function getCss(seed: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter', 'Pretendard', sans-serif;
+        font-family: 'SpoqaHanSansNeo', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: ${foreground};
